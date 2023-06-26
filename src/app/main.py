3433b87ce95e5edbfa0e03a7811d2eff4334454a
Tâmara Porfiro <77836325+tamaraporf@users.noreply.github.com>
2023-06-26 16:34:00 -0300

    PEP9 adjusted

--- src/app/main.py
+++ src/app/main.py
@@ -3,10 +3,10 @@ from flask_basicauth import BasicAuth
 from textblob import TextBlob
 from googletrans import Translator
 import pickle
-import os 
+import os
 
-colunas = ['tamanho','ano','garagem']
-modelo = pickle.load(open('../../models/modelo.sav','rb'))
+colunas = ['tamanho', 'ano', 'garagem']
+modelo = pickle.load(open('../../models/modelo.sav', 'rb'))
 
 app = Flask(__name__)
 translator = Translator()
@@ -16,9 +16,11 @@ app.config['BASIC_AUTH_PASSWORD'] = os.environ.get('BASIC_AUTH_PASSWORD')
 
 basic_auth = BasicAuth(app)
 
+
 @app.route('/')
 def home():
     return "Minha primeira API."
+
 
 @app.route('/sentimento/<frase>')
 @basic_auth.required
@@ -28,6 +30,7 @@ def sentimento(frase):
     polaridade = tb_en.sentiment.polarity
     return "polaridade: {}".format(polaridade)
 
+
 @app.route('/cotacao/', methods=['POST'])
 @basic_auth.required
 def cotacao():
@@ -35,5 +38,6 @@ def cotacao():
     dados_input = [dados[col] for col in colunas]
     preco = modelo.predict([dados_input])
     return jsonify(preco=preco[0])
+
 
 app.run(debug=True, host='0.0.0.0')
